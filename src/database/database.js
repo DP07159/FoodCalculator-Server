31f8c374b266ea18c4bbdd23be846f493f344aeb
DP07159 <Frederikdallas@gmx.de>
2026-08-05 21:29:45 +0200

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,1 +1,54 @@
+const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
+const fs = require("fs");
 
+const dataDir = "/var/data";
+
+if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir, { recursive: true });
+}
+
+const dbPath = path.join(dataDir, "food_calculator.sqlite");
+
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error("Fehler beim Öffnen der Datenbank:", err.message);
+    } else {
+        console.log(`SQLite verbunden: ${dbPath}`);
+    }
+});
+
+function run(sql, params = []) {
+    return new Promise((resolve, reject) => {
+        db.run(sql, params, function (err) {
+            if (err) reject(err);
+            else resolve(this);
+        });
+    });
+}
+
+function get(sql, params = []) {
+    return new Promise((resolve, reject) => {
+        db.get(sql, params, (err, row) => {
+            if (err) reject(err);
+            else resolve(row);
+        });
+    });
+}
+
+function all(sql, params = []) {
+    return new Promise((resolve, reject) => {
+        db.all(sql, params, (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+        });
+    });
+}
+
+module.exports = {
+    db,
+    dbPath,
+    run,
+    get,
+    all
+};
