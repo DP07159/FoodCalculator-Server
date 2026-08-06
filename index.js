3f81bdff5f43ff796346309c61cd40a3690d5369
DP07159 <Frederikdallas@gmx.de>
2026-08-06 23:38:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,6 @@ const normalizeVisibleFoodName = ingredients.normalizeVisibleFoodName;
 const normalizeGermanText = ingredients.normalizeGermanText;
 const removeIngredientDescriptors = ingredients.removeIngredientDescriptors;
 
-const singularizeFoodToken = ingredients.singularizeFoodToken;
-const titleCaseFoodToken = ingredients.titleCaseFoodToken;
-const buildFoodIdentity = ingredients.buildFoodIdentity;
-const canonicalizeIngredientName = ingredients.canonicalizeIngredientName;
-const displayIngredientNameFromCanonical =
-    ingredients.displayIngredientNameFromCanonical;
-
 const PORT = process.env.PORT || 3000;
 
 async function ensureSchema() {
