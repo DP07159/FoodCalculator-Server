1898c080b0a8be523cf40c437cb154d7d695f28a
DP07159 <Frederikdallas@gmx.de>
2026-08-06 22:39:13 +0200

    Update routes.js

--- src/modules/recipes/routes.js
+++ src/modules/recipes/routes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.get("/", recipeController.getAllRecipes);
 router.get("/:id", recipeController.getRecipeById);
+router.patch("/:id/favorite", recipeController.updateRecipeFavorite);
+router.delete("/:id", recipeController.deleteRecipe);
 
 module.exports = router;
