ae35dac1c1905064a55a308564d7572f6ab9497e
DP07159 <Frederikdallas@gmx.de>
2026-08-08 17:01:48 +0200

    Update index.js

--- src/shared/ingredients/index.js
+++ src/shared/ingredients/index.js
@@ -2,10 +2,12 @@ const units = require("./units");
 const normalizer = require("./normalizer");
 const canonicalizer = require("./canonicalizer");
 const parser = require("./parser");
+const matching = require("./matching");
 
 module.exports = {
     ...units,
     ...normalizer,
     ...canonicalizer,
-    ...parser
+    ...parser,
+    ...matching
 };
